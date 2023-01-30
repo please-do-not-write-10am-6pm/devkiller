1668c9783de5698f68c45da2290f79cd57ec02b4
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2023-01-30 14:04:04 +0100

    fixed: boilerplate

--- src/movies/components/MovieCard.tsx
+++ src/movies/components/MovieCard.tsx
@@ -1,45 +1,42 @@
-import { StarRating, Button } from 'shared/components';
+import { StarRating, Button } from "shared/components";
 
-import { getAvgRating } from 'movies/ratings';
-import { Movie } from 'types';
-import { useMovies } from './MovieProvider';
+import { getAvgRating } from "movies/ratings";
+import { Movie } from "types";
+import { useMovies } from "./MovieProvider";
 
 interface MovieCardProps {
-    movie: Movie,
+  movie: Movie;
 }
 
 export const MovieCard = ({ movie }: MovieCardProps) => {
-    const { moviesDispatch } = useMovies();
+  const { moviesDispatch } = useMovies();
 
-    // TODO: implement required functionality
+  // TODO: implement required functionality
+  const handleDelete = () => {};
 
-    return (
-        <div data-testid={`movie-item-${movie.id}`}>
-            {/* TODO: Display image */}
-            <img className="card-img-top" alt="" />
-            <div className="card-body">
-                <h4 className="card-title">
-                    {/* TODO: Display title */}
-                </h4>
-                <h6 className="card-subtitle mb-2 text-muted">
-                    {/* TODO: Display subtitle */}
-                </h6>
-                <p className="text-justify" style={{ fontSize: '14px' }}>
-                    {/* TODO: Display description */}
-                </p>
-                {/* TODO: Implement delete functionality */}
-                <Button>Delete</Button>
-            </div>
-            <div className="card-footer">
-                <div className="clearfix">
-                <div className="float-left mt-1">
-                    {/* TODO: Display stars */}
-                </div>
-                <div data-testid="movie-rating" className="card-footer-badge float-right badge badge-primary badge-pill">
-                    {/* TODO: Display rating value */}
-                </div>
-                </div>
-            </div>
-        </div>    
-    )
+  return (
+    <div data-testid={`movie-item-${movie.id}`}>
+      {movie.imageUrl}
+      <img className="card-img-top" alt="" />
+      <div className="card-body">
+        <h4 className="card-title">{movie.title}</h4>
+        <h6 className="card-subtitle mb-2 text-muted">{movie.subtitle}</h6>
+        <p className="text-justify" style={{ fontSize: "14px" }}>
+          {movie.description}
+        </p>
+        <Button onClick={handleDelete}>Delete</Button>
+      </div>
+      <div className="card-footer">
+        <div className="clearfix">
+          <div className="float-left mt-1">{/* TODO: Display stars */}</div>
+          <div
+            data-testid="movie-rating"
+            className="card-footer-badge float-right badge badge-primary badge-pill"
+          >
+            {/* TODO: Display rating value */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/movies/useMoviesReducer.tsx
+++ src/movies/useMoviesReducer.tsx
@@ -1,33 +1,55 @@
-import React, { useReducer, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
+import React, { useReducer, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 
-import { Movie, MoviesAction } from 'types';
-import { getMovies } from 'api/movies';
+import { Movie, MoviesAction } from "types";
+import { getMovies } from "api/movies";
 
 interface MoviesState {
-  movies: Movie[]
-  initialized: boolean
+  movies: Movie[];
+  initialized: boolean;
 }
 
-export function useMoviesReducer(): [MoviesState, React.Dispatch<MoviesAction>] {
-  // TODO: Implement all action processing
-
-  const movieReducer = (state: MoviesState, action: MoviesAction): MoviesState => {
+export function useMoviesReducer(): [
+  MoviesState,
+  React.Dispatch<MoviesAction>
+] {
+  const movieReducer = (
+    state: MoviesState,
+    action: MoviesAction
+  ): MoviesState => {
     switch (action.type) {
-      case 'fetch':
-        return { ...state };
+      case "fetch":
+        return { ...state, movies: action.payload.data };
 
-      case 'add':
-        return { ...state };
+      case "add":
+        return {
+          ...state,
+          movies: [
+            ...state.movies,
+            { id: uuid(), ratings: [], ...action.payload.movie },
+          ],
+        };
 
-      case 'delete':
-        return { ...state };
+      case "delete":
+        return {
+          ...state,
+          movies: state.movies.filter(
+            (movie) => movie.id !== action.payload.movieId
+          ),
+        };
 
-      case 'rate':
-        return { ...state };
+      case "rate":
+        return {
+          ...state,
+          movies: state.movies.map((movie) =>
+            movie.id === action.payload.movieId
+              ? { ...movie, ratings: [...movie.ratings, action.payload.rating] }
+              : movie
+          ),
+        };
 
       default:
-        return state
+        return state;
     }
   };
 
@@ -37,7 +59,9 @@ export function useMoviesReducer(): [MoviesState, React.Dispatch<MoviesAction>] 
   });
 
   useEffect(() => {
-    // TODO: Call fetch action
+    getMovies().then((movies) => {
+      dispatch({ type: "fetch", payload: { data: movies } });
+    });
   }, []);
 
   return [state, dispatch];
